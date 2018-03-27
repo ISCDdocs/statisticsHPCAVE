9fd95bb510d329157f24d8965d83e8dcdd3a65c3
root <root@debian>
2018-03-27 16:38:11 +0200

    Updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# statisticsHPCAVE
-les scripts à éxécuter sur les frontales de MeSU se trouvent dans la branche **frontales**, et ceux à éxécuter sur le serveur web ainsi que le code javascript à lancer sur la page se trouvent dans la branche **serveurWeb**.
+# dashboardHPCAVE
+Scripts pour avoir une mise à jour du statut des machines de calcul en "temps réel" depuis le site hpcave.upmc.fr.
 
-Des informations pour installer ce "système" sont données dans [la page du site associée](https://iscddocs.github.io/docs/hpcave/statistics.html)
+Des informations complémentaires sont présentes dans [la page du site associée](https://iscddocs.github.io/docs/hpcave/statistics.html)
--- statsMesu.js
+++ statsMesu.js
@@ -1,16 +1,29 @@
-var dataPath = "/statisticsHPCAVE/logs/";
-var imgsPath = "/statisticsHPCAVE/imgs/"
+var dataPath = "/dashboardHPCAVE/logs/";
+var imgsPath = "/dashboardHPCAVE/images/";
+
+//Transform a usage percentage in a color for the badges
+function percentToColor(percent){
+    if(percent<10){return "#800"}
+    if(percent>=10 && percent<25){return "#840"}
+    if(percent>=25 && percent<40){return "#880"}
+    if(percent>=40 && percent<55){return "#8a0"}
+    if(percent>=55 && percent<70){return "#480"}
+    if(percent>=70 && percent<100){return "#080"}
+}
 
 function fillPings(text){
-  lines = text.split("\n");
-  for ( var i = 0 ; i < lines.length ; i++ ){
-    if (lines[i].split(",")[1] == "1"){
-      jQuery("#mesu" + i.toString()).attr("src", imgsPath + "on" + i.toString() + ".svg");
-    }else{
-      jQuery("#mesu" + i.toString()).attr("src", imgsPath + "off" + i.toString() + ".svg");
+    lines = text.split("\n");
+    for ( var i = 0 ; i < lines.length ; i++ ){
+	if(lines[i].length > 3){
+	    if (lines[i].split(",")[1].includes("1")){
+		jQuery("#mesu" + (i+1).toString()).attr("src", imgsPath + "on" + (i+1).toString() + ".svg");
+	    }else{
+		jQuery("#mesu" + (i+1).toString()).attr("src", imgsPath + "off" + (i+1).toString() + ".svg");
+	    }
+	}
     }
-  }
 }
+
 function fillStatus(text){
   lines = text.split("\n");
   if(lines.length>1){
@@ -20,8 +33,10 @@ function fillStatus(text){
     var runningBeta  = lines[3].split("= ")[1].split(" ")[1]
     var queuedAlpha  = lines[2].split("= ")[1].split(" ")[0]
     var queuedBeta   = lines[2].split("= ")[1].split(" ")[1]
-    jQuery("#pourcentageAlpha").html(alphaPercent.toString());
-    jQuery("#pourcentageBeta" ).html(betaPercent.toString());
+      jQuery("#pourcentageAlpha").html(alphaPercent.toString());
+      jQuery("#pourcentageAlpha").parent().css("color", percentToColor(alphaPercent));
+      jQuery("#pourcentageBeta" ).html(betaPercent.toString());
+      jQuery("#pourcentageBeta").parent().css("color", percentToColor(betaPercent));
     jQuery("#runningAlpha").html(    runningAlpha.toString());
     jQuery("#runningBeta").html(     runningBeta.toString());
     jQuery("#queuedAlpha").html(     queuedAlpha.toString());
@@ -31,9 +46,9 @@ function fillStatus(text){
 }
 function fillLastMonth(text){
   lines = text.split("\n");
-  var nUsers = lines.length;
+  var nUsers = lines.length - 1;
   var cpu = 0;
-  for(var i = 0 ; i < lines.length ; i++){
+  for(var i = 0 ; i < lines.length - 1 ; i++){
     cpu = cpu + parseInt( (lines[i].split(","))[1] );
   }
   jQuery("#nUsersMonth").html(nUsers.toString());
@@ -41,24 +56,26 @@ function fillLastMonth(text){
 }
 function fillLastYear(text){
   lines = text.split("\n");
-  var nUsers = lines.length;
+  var nUsers = lines.length - 1;
   var cpu = 0;
-  for(var i = 0 ; i < lines.length ; i++){
+    for(var i = 0 ; i < lines.length - 1 ; i++){
+	console.log("ligne =", lines[i])
     cpu = cpu + parseInt( (lines[i].split(","))[1] );
   }
   jQuery("#nUsersYear").html(nUsers.toString());
   jQuery("#nCpuYear").html(cpu.toString());
 }
-function fillInteruptions(text){
+function fillInterruptions(text){
   lines = text.split("\n");
   html = "";
-  for (var i = 0 ; i < lines.length ; i++){
+  for (var i = 0 ; i < lines.length - 1 ; i++){
     for (var j = 0 ; j < 6 ; j++){
-      if (lines[1].split(",")[0].includes(j.toString())){
-        html+="<b>mesu" + j.toString() + ": </b> " + lines[i].split(",")[1] + "<br>";
+      if (lines[i].split(",")[0].includes(j.toString())){
+          html+="<b>mesu" + j.toString() + ": </b> " + lines[i].split(",")[1].split(" days")[0] + " days<br>";
       }
     }
   }
+  jQuery("#interruptions").html(html);
 }
 
 jQuery.ajax({type:"GET", url: dataPath + "pings.csv",     success: fillPings});
